06906b988465ddaed4465c5f0866a535a4c0826f
acheng98 <alcheng@mit.edu>
2018-01-02 13:59:32 -0700

    hm

--- scripts.js
+++ scripts.js
@@ -2,9 +2,3 @@ function change_tab(tab_name) {
 	$('#Home, #Coursework, #Projects, #Misc').hide();
 	$(tab_name).fadeIn(500);
 }
-
-function main() {
-	$('#Home, #Coursework, #Projects, #Misc').hide();
-	$('#main-content').show();
-	$('#Home').show();
-}
