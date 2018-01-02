ffbf02c4190f9c68f3b49c4cc5f34c41de1c8890
acheng98 <alcheng@mit.edu>
2018-01-02 14:03:31 -0700

    outback

--- scripts.js
+++ scripts.js
@@ -2,3 +2,8 @@ function change_tab(tab_name) {
 	$('#Home, #Coursework, #Projects, #Misc').hide();
 	$(tab_name).fadeIn(500);
 }
+
+function main() {
+	$('#Home, #Coursework, #Projects, #Misc').hide();
+	$('#Home').show();
+}
